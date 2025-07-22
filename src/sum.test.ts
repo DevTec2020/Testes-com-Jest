d0cd4a8fb97dd71a00a8276ba2c5c31b07b6707b
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-07-22 19:07:40 -0300

    feat: Agrupando testes

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,8 +1,17 @@
 import { sum } from "./server"
 
-test("sum of 3 + 7 must be 10", () => {
-    const result = sum(3, 7)
-
-    expect(result).toBe(10)
+describe ("sum", () => {
+    test("sum of 3 + 7 must be 10", () => {
+        const result = sum(3, 7)
+    
+        expect(result).toBe(10)
+    
+    })
+    
+    test("sum of 2 + 2 most be 4", () => {
+        const result = sum(2,2)
+    
+        expect(result).toBe(4)
+    })
 
 })
